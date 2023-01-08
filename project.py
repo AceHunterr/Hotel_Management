2d682550f9246643387cd34544ffc9030a14e26b
Maanas <soomaanas7@gmail.com>
2023-01-08 20:03:16 +0530

    added bye

--- project.py
+++ project.py
@@ -1,28 +1,33 @@
+import math
 import csv
 
-############### DATA HANDLING ######################### 
+############### DATA HANDLING #########################
+
 
 def LoadDetails(csv_file):
     # """Reading data from the CSV File and then converting it into list of lists"""
 
     with open(csv_file, 'r') as file:
-        readCSV = csv.reader(file,delimiter=',')
-        
+        readCSV = csv.reader(file, delimiter=',')
+
         # The first row i.e. index=0 in the list of lists would be the header row that contains the title fields... therefore our room categories should start from index 1 not 0
         rooms_data_list = [row for row in readCSV]
         return rooms_data_list
 
+
 def enhance_room_details(list):
     # """For converting the numeric data which is in string format to integer format for calculations"""
 
-    return [int(item) if list.index(item)!=0 else item for item in list]
+    return [int(item) if list.index(item) != 0 else item for item in list]
+
 
 def to_string_room_details(list):
     # """For converting all the data back to string format to write in the file"""
 
     return [str(item) for item in list]
 
-def UpdateTextFile(roomData,file):
+
+def UpdateTextFile(roomData, file):
     # """Writing the updated data recieved back in to the file"""
 
     with open(file, 'w', encoding='UTF8', newline='') as f:
@@ -31,17 +36,13 @@ def UpdateTextFile(roomData,file):
             writer.writerow(row)
 
 
+############### HOTEL MANAGER FUNCTIONALITY #########################
 
-############### HOTEL MANAGER FUNCTIONALITY ######################### 
+dash = "------------------------------------------------"
 
-import math
-   
 
-dash =  "------------------------------------------------"
-         
-def CheckService(roomData,id):
+def CheckService(roomData, id):
     # """To Check whether the service is to make reservation or checkout"""
-
 
     # Entering Category
     cat_input = input(""" Select from the room's category
@@ -57,11 +58,12 @@ Your Choice> """)
     # Checking for Invalid Input Error using Try and Except
     try:
         category = int(cat_input)
-        if not category in range(1,7):  
-            print("\n!!!YOU ENTERED INVALID DATA!!!\n") 
-            return None 
-    except ValueError: 
-        print("\n!!!YOU ENTERED INVALID DATA!!!\n") # None is returned in case user enters invalid input and raises a ValueError 
+        if not category in range(1, 7):
+            print("\n!!!YOU ENTERED INVALID DATA!!!\n")
+            return None
+    except ValueError:
+        # None is returned in case user enters invalid input and raises a ValueError
+        print("\n!!!YOU ENTERED INVALID DATA!!!\n")
         return None
 
     # Entering the number of guests
@@ -72,19 +74,18 @@ How many guests present? > """)
     except ValueError:
         print("\n!!!YOU ENTERED INVALID DATA!!!\n")
         return None
-    
+
     room_details = roomData[category]
     room_details = enhance_room_details(room_details)
 
-    if id==1:
+    if id == 1:
 
         # Checking Availability
-        is_available = CheckAvailability(roomData,category,numberOfGuests)
-            
+        is_available = CheckAvailability(roomData, category, numberOfGuests)
 
         if is_available:
-            print("\nGreat. We have availability.")        
-            # Entering the nights to stay 
+            print("\nGreat. We have availability.")
+            # Entering the nights to stay
             dur_input = input("""
 How many nights would you like to stay in for? > """)
             try:
@@ -92,24 +93,26 @@ How many nights would you like to stay in for? > """)
             except ValueError:
                 print("\n!!!YOU ENTERED INVALID DATA!!!\n")
                 return None
-        
-                
+
             # Calculating Price
-            price = CalculatePrice(roomData,durationOfStay,category,numberOfGuests) 
-            print(f"\n{dash}\nYour Total Price for the rooms would be: {price}\n{dash}\n")
+            price = CalculatePrice(
+                roomData, durationOfStay, category, numberOfGuests)
+            print(
+                f"\n{dash}\nYour Total Price for the rooms would be: {price}\n{dash}\n")
             print("Thank You for your Patience. Your Reservation has been made\n")
-            
-            # Updating the data according to making reservation or checking out... id=1 : MakeReservation, id=2 :CheckOut 
-            UpdateData(room_details,numberOfGuests,category,roomData,id=id)
+
+            # Updating the data according to making reservation or checking out... id=1 : MakeReservation, id=2 :CheckOut
+            UpdateData(room_details, numberOfGuests, category, roomData, id=id)
 
         else:
-            print("\n!!!Sorry , Currently we are out of vacancies for the entered room!!!\n")
-    
+            print(
+                "\n!!!Sorry , Currently we are out of vacancies for the entered room!!!\n")
+
     else:
-        UpdateData(room_details,numberOfGuests,category,roomData,id=id)
+        UpdateData(room_details, numberOfGuests, category, roomData, id=id)
 
 
-def CheckAvailability(roomData,category,numberOfGuests):
+def CheckAvailability(roomData, category, numberOfGuests):
     # """Checking the Availability depending by calculating the vacancies using occupied and unoccupied"""
 
     room_details = roomData[category]
@@ -118,16 +121,16 @@ def CheckAvailability(roomData,category,numberOfGuests):
     capacity = room_details[2]
     rooms_occupied = room_details[6]
     vacant_rooms = num_rooms-rooms_occupied
-    vacant_capacity = vacant_rooms*capacity 
+    vacant_capacity = vacant_rooms*capacity
 
-    if vacant_capacity >=numberOfGuests:
+    if vacant_capacity >= numberOfGuests:
         return True
 
     else:
         return False
 
 
-def CalculatePrice(roomData,durationOfStay,roomIndex,numberOfGuests):
+def CalculatePrice(roomData, durationOfStay, roomIndex, numberOfGuests):
     # """Calculate the total price according to the numbers of days(which include the different prices for days,weeks and months) and rooms booked"""
 
     room_details = roomData[roomIndex]
@@ -139,48 +142,51 @@ def CalculatePrice(roomData,durationOfStay,roomIndex,numberOfGuests):
 
     # Calculating rooms needed by total guests
     int_rooms = int(numberOfGuests/capacity)
-    rooms_needed = (math.floor(int_rooms)+1) if (numberOfGuests%capacity!=0) else int_rooms 
+    rooms_needed = (math.floor(int_rooms)+1) if (numberOfGuests %
+                                                 capacity != 0) else int_rooms
 
-    # Calculating the price acc to price per month,week and day 
+    # Calculating the price acc to price per month,week and day
     months = math.floor(durationOfStay/30)
-    days_left = durationOfStay%30
+    days_left = durationOfStay % 30
     weeks = math.floor(days_left/7)
-    days = days_left%7
-    total_cost = ((months*per_month) + (weeks*per_week) + (days*per_night)*rooms_needed)
+    days = days_left % 7
+    total_cost = ((months*per_month) + (weeks*per_week) +
+                  (days*per_night)*rooms_needed)
 
     return total_cost
 
 
-def UpdateData(room_details,numberOfGuests,category,roomData,id=1):
+def UpdateData(room_details, numberOfGuests, category, roomData, id=1):
     # """Updating our roomData by changing the num_rooms_occupied field i.e. room_details[6] by adding (during reservation) or subtracting (during Checkout) the rooms in the field """
-    
+
     capacity = room_details[2]
     int_rooms = int(numberOfGuests/capacity)
-    rooms_needed = (math.floor(int_rooms)+1) if (numberOfGuests%capacity!=0) else int_rooms 
+    rooms_needed = (math.floor(int_rooms)+1) if (numberOfGuests %
+                                                 capacity != 0) else int_rooms
 
-    if id ==1:
+    if id == 1:
         # During Making Reservation
         room_details[6] += rooms_needed
 
     else:
         # During Checking Out
-        if rooms_needed>room_details[6]:
+        if rooms_needed > room_details[6]:
             # If the Checking Out Overflows the total number of rooms present then it returns the error message
             print("\nYou have entered WRONG CHECKOUT DATA!!! TRY AGAIN\n")
-        else: 
+        else:
             room_details[6] -= rooms_needed
             print("\nThank You for staying at Fancy Hotel. Please Visit Again\n")
-            
 
     room_details = to_string_room_details(room_details)
     roomData[category] = room_details
+
 
 def CreateSummary(roomData):
     # """Prints the Room Category and the number of vacancies available in each room"""
 
     print("\n")
     for row in roomData:
-        if roomData.index(row)!=0:
+        if roomData.index(row) != 0:
             print(f"{row[0]} | {int(row[1]) - int(row[6])} rooms are available")
     print("\n")
 
@@ -197,24 +203,25 @@ print("\nWelcome to Fancy Hotel... Please select from the following services\n")
 while not quit:
     # While user want to keeps entering data the process goes on
 
-    service = input("\nEnter 1 to make a new Reservation, 2 to Check out or 'q' to quit , 's' to see rooms available : ").lower()
-    
+    service = input(
+        "\nEnter 1 to make a new Reservation, 2 to Check out or 'q' to quit , 's' to see rooms available : ").lower()
+
     if service == "1":
-        # Creating NEW RESERVATION 
+        # Creating NEW RESERVATION
 
         print(f"\n{stars}\nLets Begin the Registration Process\n{stars}\n")
-        CheckService(roomData,id=1)
+        CheckService(roomData, id=1)
         print(f"\n{stars}\n")
 
     elif service == "2":
         # CHECKING OUT the customer
 
         print(f"\n{stars}\nLets Begin the Checkout Process\n{stars}")
-        CheckService(roomData,id=2)
+        CheckService(roomData, id=2)
         print(f"\n{stars}\n")
 
     elif service == 'q' or service == "quit":
-        #Ending the process 
+        # Ending the process
 
         quit = True
 
@@ -229,10 +236,10 @@ while not quit:
     if not quit:
         # If the user wants to continue with services can continue by typing 'y'
         if (input("Do you want to continue with our services? Enter 'y' to continue otherwise enter: ").lower()) != 'y':
-            quit=True
+            quit = True
 
-UpdateTextFile(roomData,file)
+UpdateTextFile(roomData, file)
 CreateSummary(roomData)
+print("BYE")
 
 print(f"{hash}\n !!GOODBYE!!\n{hash}")
-
